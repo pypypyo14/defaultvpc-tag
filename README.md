f87ab09c9ee128b84741a05e7b6961283e00c45d
Saori <jkmr9214@gmail.com>
2020-01-10 01:01:10 +0900

    --profile オプションの追加 & ログにリージョン情報を出力

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # defaultvpc-tag
 
-## ABOUT
+```
+$ python3 main.py -h
+usage: main.py [-h] [-d] [--profile PROFILE]
+
+Add AWS tag {isDefault: true} to default EC2 & RDS resources
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -d, --dryrun       set dry_run flag
+  --profile PROFILE  set aws profile
+```
+
 - AWSアカウント作成時にデフォルトで存在するリソースにタグ付け(`{isDefault: true}`)するスクリプト
   - Default VPCとそれに紐づく以下のリソース
     - Subnet*
@@ -13,14 +24,3 @@
 - 手動作成したDefault VPCにもタグが付く
 - Default VPC内に手動で作った上記`*`印のリソースにもタグが付く
   - 厳密に管理したい場合は本スクリプトのDryRunや本スクリプト実行後にタグエディタなどから対象の確認をおすすめします
-
-## USAGE
-```
-usage: main.py [-h] [-d]
-
-Add AWS tag {isDefault: true} to default EC2 & RDS resources
-
-optional arguments:
-  -h, --help    show this help message and exit
-  -d, --dryrun  set dry_run flag
-```
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-import boto3, argparse
+import boto3, argparse, sys
 from botocore.exceptions import ClientError
 
+PROFILE = ''
 IS_DRY_RUN = False
 TAG_KEY = 'isDefault'
 TAG_VALUE = 'true'
-REGIONS = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
 
 def parse_arg():
-    global IS_DRY_RUN
+    global IS_DRY_RUN, PROFILE
     parser = argparse.ArgumentParser(description='Add AWS tag {isDefault: true} to default EC2 & RDS resources')
     parser.add_argument('-d', '--dryrun', action='store_true', help='set dry_run flag')
+    parser.add_argument('--profile', action='store', help='set aws profile')
+    profile = parser.parse_args().profile
+    PROFILE = 'default' if profile is  None else profile
     IS_DRY_RUN = parser.parse_args().dryrun
 
-def query_default_vpc(ec2):
+def query_default_vpc(ec2, region):
     default_vpc = ec2.describe_vpcs(
         Filters=[{ 'Name': 'isDefault', 'Values': ['true']}]
     )
     if(len(default_vpc['Vpcs']) == 0):
-        print('EC2: Default VPC not found.')
+        print('INFO:EC2:' + region + ': Default VPC not found.')
         return None
     return default_vpc['Vpcs'][0]['VpcId']
 
-def query_defaultvpc_ec2_resources(ec2, vpc):
+def query_defaultvpc_ec2_resources(ec2, vpc, region):
     default_vpc_condition = [{
         'Name': 'vpc-id',
         'Values': [vpc]
@@ -51,17 +54,16 @@ def query_defaultvpc_ec2_resources(ec2, vpc):
     default_vpc_resources.extend(generate_resourceid_list(response, 'InternetGateways', 'InternetGatewayId'))
 
     # DHCP optionset
-    default_domain_names=[region + '.compute.internal' for region in REGIONS] + ['ec2.internal']
     response = ec2.describe_dhcp_options(Filters=[
         {'Name': 'key', 'Values': ['domain-name']},
-        {'Name': 'value', 'Values': default_domain_names}
+        {'Name': 'value', 'Values': [region+'.compute.internal','ec2.internal'] }
     ])
     default_vpc_resources.extend(generate_resourceid_list(response, 'DhcpOptions', 'DhcpOptionsId'))
 
     return default_vpc_resources
 
-def add_ec2_tag (ec2, resourcelist, tagkey, tagvalue):
-    print('EC2: ' + ','.join(resourcelist))
+def add_ec2_tag (ec2, resourcelist, tagkey, tagvalue, region):
+    print('INFO:EC2:' + region + ': Tag on '+ ','.join(resourcelist))
     try:
         ec2.create_tags(
             DryRun=IS_DRY_RUN,
@@ -70,30 +72,30 @@ def add_ec2_tag (ec2, resourcelist, tagkey, tagvalue):
         )
     except ClientError as e:
         if 'DryRunOperation' == e.response['Error']['Code']:
-            print('EC2: '+ e.response['Error']['Message'])
+            print('INFO:EC2:' + region + ': '+ e.response['Error']['Message'])
         else:
             raise
     else:
-        print('EC2: done')
+        print('INFO:EC2:' + region + ': Success!')
 
 def generate_resourceid_list(response, top_key, resourceid_key):
     return [item[resourceid_key] for item in response[top_key]]
 
-def query_rds_defaultsg_list(rds):
+def query_rds_defaultsg_list(rds, region):
     try:
         response =  rds.describe_db_security_groups(DBSecurityGroupName='default')
     except ClientError as e:
         if 'DBSecurityGroupNotFound' == e.response['Error']['Code']:
-            print('RDS: '+ e.response['Error']['Message'])
+            print('INFO:RDS:' + region + ': ' + e.response['Error']['Message'])
         else:
             raise
     else:
         return response['DBSecurityGroups'][0]
 
-def add_rds_tag (rds, resource_arn, tagkey, tagvalue):
-    print('RDS: ' + resource_arn)
+def add_rds_tag (rds, resource_arn, tagkey, tagvalue, region):
+    print('INFO:RDS:' + region + ': Tag on ' + resource_arn)
     if(IS_DRY_RUN):
-        print('RDS: Request would have succeeded, but DryRun flag is set.')
+        print('INFO:RDS:'+ region +': Request would have succeeded, but DryRun flag is set.')
         return
     else:
         try:
@@ -104,28 +106,31 @@ def add_rds_tag (rds, resource_arn, tagkey, tagvalue):
         except:
             raise
         else:
-            print('RDS: done')
+            print('INFO:RDS:' + region + ': Success!')
 
 def create_clients():
-    ec2 = [boto3.client('ec2',region_name=region) for region in REGIONS]
-    rds = [boto3.client('rds',region_name=region) for region in REGIONS]
-    return {
-        'ec2': ec2,
-        'rds': rds
-    }
+    try:
+        session = boto3.Session(profile_name=PROFILE)
+    except Exception as e:
+        print(e)
+        sys.exit()
+    regions = [region['RegionName'] for region in session.client('ec2', region_name='us-east-1').describe_regions()['Regions']]
+    ec2 = [session.client('ec2',region_name=region) for region in regions]
+    rds = [session.client('rds',region_name=region) for region in regions]
+    return list(zip(regions, ec2, rds))
 
 def main():
     parse_arg()
     clients = create_clients()
-    for ec2 in clients['ec2']:
-        vpc = query_default_vpc(ec2)
+    for client in clients:
+        region, ec2, rds = client
+        vpc = query_default_vpc(ec2, region)
         if (vpc):
-            default_vpc_resources = query_defaultvpc_ec2_resources(ec2, vpc)
-            add_ec2_tag(ec2, default_vpc_resources, TAG_KEY, TAG_VALUE)
-    for rds in clients['rds']:
-        rds_default_sg = query_rds_defaultsg_list(rds)
+            default_vpc_resources = query_defaultvpc_ec2_resources(ec2, vpc, region)
+            add_ec2_tag(ec2, default_vpc_resources, TAG_KEY, TAG_VALUE, region)
+        rds_default_sg = query_rds_defaultsg_list(rds,region)
         if(rds_default_sg):
-            add_rds_tag(rds, rds_default_sg['DBSecurityGroupArn'], TAG_KEY, TAG_VALUE)
+            add_rds_tag(rds, rds_default_sg['DBSecurityGroupArn'], TAG_KEY, TAG_VALUE,region)
 
 if __name__ == "__main__":
     main()
